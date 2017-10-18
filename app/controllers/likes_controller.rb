11316ca9ef0b0e7a4b26d46c4d9194552e9a550e
Hui Qian Liang <liang.huiqian@gmail.com>
2017-10-18 16:35:31 +0800

    Implemented AJAX for photo likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,21 @@ class LikesController < ApplicationController
     @user = current_user
     @photo = Photo.find_by(id: params[:photo_id])
     @photo.like(@user)
-    redirect_to photo_path(@photo)
+    flash.now[:success] = "Photo liked"
+    respond_to do |f|
+      f.js
+      f.html { redirect_to photo_path(@photo) }
+    end
   end
 
-  def delete
+  def destroy
     @user = current_user
     @photo = Photo.find_by(id: params[:photo_id])
     @photo.unlike(@user)
-    redirect_to photo_path(@photo)
+    flash.now[:danger] = "Photo unliked"
+    respond_to do |f|
+      f.js
+      f.html { redirect_to photo_path(@photo) }
+    end
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,6 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :photo
 
-  validates :user_id, uniqueness: {scope: :photo_id}
-  
+  validates :user_id, uniqueness: { scope: :photo_id }
+
 end
